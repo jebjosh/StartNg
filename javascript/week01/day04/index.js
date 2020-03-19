8bf90ff7bd5f9a4d06e985fbe1716b73983ec99a
Jeb <Jebjosh@gmail.com>
2020-03-19 19:53:20 -0400

    js day 4

--- javascript/week01/day04/index.js
+++ javascript/week01/day04/index.js
@@ -1,7 +1,7 @@
 for (i = 1; i <= 5; i++) {
   console.log("*".repeat(i));
   if (i == 5) {
-    for (j = 4; j >= 1; j--) {
+    for (j = i - 1; j >= 1; j--) {
       console.log("*".repeat(j));
     }
   }
